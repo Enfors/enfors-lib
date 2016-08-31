d993bcef90080d6441420b8177acfcd15fcdaa34
Christer Enfors <christer.enfors@gmail.com>
2016-08-31 09:13:22 +0000

    Move paredit setup to a separate file.

--- dot-emacs.el
+++ dot-emacs.el
@@ -34,17 +34,6 @@
 
 (show-paren-mode 1)			; Always show matching parens.
 
-;; Load paredit
-;; (load-file "~/lib/elisp/paredit.el")
-;; (autoload 'enable-paredit-mode "paredit"
-;;   "Turn on pseudo-structural editing of Lisp code." t)
-;; (add-hook 'emacs-lisp-mode-hook                  #'enable-paredit-mode)
-;; (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
-;; (add-hook 'ielm-mode-hook                        #'enable-paredit-mode)
-;; (add-hook 'lisp-mode-hook                        #'enable-paredit-mode)
-;; (add-hook 'lisp-interaction-mode-hook            #'enable-paredit-mode)
-;; (add-hook 'scheme-mode-hook                      #'enable-paredit-mode)
-
 ;; Load enfors-lib
 (or (boundp 'enfors-path)
     (setq enfors-path "/home/enfors/devel/elisp/enfors-lib"))
