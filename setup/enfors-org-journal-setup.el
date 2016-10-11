36e5b271859ef77c08388319335ee952e6590c26
Christer Enfors <christer.enfors@gmail.com>
2016-10-11 22:59:58 +0200

    Initial version.

---
+++ setup/enfors-org-journal-setup.el
@@ -0,0 +1,11 @@
+;;;; enfors-org-journal-setup.el - Set up org-journal
+;;;; Get org-journal with M-x package-install org-journal.
+;;;;
+;;;; Add a new journal entry with C-c C-j.
+;;;;
+;;;; Entries are viewable from the calendar.
+;;;; j:   view entry
+;;;; C-j: view entry but don't switch to it
+(require 'org-journal)
+(setq org-journal-dir         "~/priv/journal/"
+      org-journal-date-format "%x: %A")
