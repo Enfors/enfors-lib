10f256fbd6a5e6a2c0d4fc87dae0073ec95194e7
Christer Enfors <christer.enfors@gmail.com>
2016-10-16 17:21:45 +0200

    Add enfors-practice-math.

--- toys.el
+++ toys.el
@@ -11,3 +11,42 @@
 	(insert "\\")))			;       else, insert (escaped) "\"
     (insert "\n")))			;     End of line, insert newline.
 
+(defun enfors-practice-math (max)
+  "Practice multiplication."
+  (interactive "nEnter max: ")
+  (message "Max is %d." max)
+  (let ((num-questions 4)
+	(a 0)
+	(b 0)
+	(answer 0)
+	(correct-answers 0))
+    (dotimes (iter num-questions)
+      (setq a (+ 2 (random (- max 1)))
+	    b (+ 2 (random (- max 1))))
+      (insert (format "Question %2d: %2d * %2d = " (+ 1 iter) a b))
+      (setq answer (read-number "Enter answer: "))
+      (insert (format "%d - " answer))
+      (if (eq answer (* a b))
+	  (progn
+	    (insert "correct.\n")
+	    (setq correct-answers (+ 1 correct-answers)))
+	(insert (format "no, the answer is %d.\n"
+			(* a b)))))
+    (insert (format "You got %d out of %d correct.\n"
+		    correct-answers num-questions))))
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
