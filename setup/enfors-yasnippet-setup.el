ecd87e8058723f0ff97abf1b558d52cc108a042d
Christer Enfors <christer.enfors@gmail.com>
2016-10-18 21:44:49 +0200

    Enable yas-global-mode by default.

--- setup/enfors-yasnippet-setup.el
+++ setup/enfors-yasnippet-setup.el
@@ -5,5 +5,6 @@
       (add-to-list 'load-path
 		   "~/.emacs.d/plugins/yasnippet")
       (require 'yasnippet)
+      (yas-global-mode 1)
       (provide 'enfors-yasnippet-setup))
   (message "yasnippet doesn't appear to be installed."))
