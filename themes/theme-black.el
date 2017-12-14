69574febfba2d3fac66be609424d87820ec90552
Christer Enfors <christer.enfors@gmail.com>
2017-12-14 10:34:04 +0100

    Set background color and default foreground
    This was for use in the graphical version of Emacs, which has
    a white background by default.

--- themes/theme-black.el
+++ themes/theme-black.el
@@ -2,8 +2,11 @@
 (set-foreground-color "white")
 
 ;;;; Faces
+(set-face-foreground 'default                      "white")
 (set-face-foreground 'font-lock-string-face        "yellow")
 (set-face-foreground 'font-lock-keyword-face       "magenta")
 (set-face-foreground 'font-lock-comment-face       "cyan")
 (set-face-foreground 'font-lock-variable-name-face "green")
 (set-face-foreground 'font-lock-constant-face      "green")
+(set-background-color                              "black")
+
