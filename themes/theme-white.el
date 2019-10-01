7127d76db0e8128d30dbfc3aaab845edfb2829fd
Christer Enfors <christer.enfors@gmail.com>
2019-10-01 13:54:00 +0000

    Add white theme

---
+++ themes/theme-white.el
@@ -0,0 +1,10 @@
+(set-background-color "white")
+(set-foreground-color "black")
+
+;;;; Faces
+(set-face-foreground 'default                      "black")
+(set-face-foreground 'font-lock-string-face        "orange")
+(set-face-foreground 'font-lock-keyword-face       "magenta")
+(set-face-foreground 'font-lock-comment-face       "#8888bb")
+(set-face-foreground 'font-lock-variable-name-face "green")
+(set-face-foreground 'font-lock-constant-face      "green")
