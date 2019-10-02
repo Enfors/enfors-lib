966c8526b20084e2c9d61770c0cde3eb01fda012
Christer Enfors <christer.enfors@gmail.com>
2019-10-02 10:37:05 +0200

    Add blue theme

---
+++ themes/theme-blue.el
@@ -0,0 +1,10 @@
+(set-background-color "#aaccee")
+(set-foreground-color "darkblue")
+
+;;;; Faces
+(set-face-foreground 'default                      "darkblue")
+(set-face-foreground 'font-lock-string-face        "yellow")
+(set-face-foreground 'font-lock-keyword-face       "darkgreen")
+(set-face-foreground 'font-lock-comment-face       "darkcyan")
+(set-face-foreground 'font-lock-variable-name-face "green")
+(set-face-foreground 'font-lock-constant-face      "green")
