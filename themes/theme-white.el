22f68be11ea9ad4d02ee79fb31e96af14dac447d
Christer Enfors <christer.enfors@gmail.com>
2019-10-02 10:36:44 +0200

    Make keywords, variable names and constants darker

--- themes/theme-white.el
+++ themes/theme-white.el
@@ -4,7 +4,7 @@
 ;;;; Faces
 (set-face-foreground 'default                      "black")
 (set-face-foreground 'font-lock-string-face        "orange")
-(set-face-foreground 'font-lock-keyword-face       "magenta")
+(set-face-foreground 'font-lock-keyword-face       "darkmagenta")
 (set-face-foreground 'font-lock-comment-face       "#8888bb")
-(set-face-foreground 'font-lock-variable-name-face "green")
-(set-face-foreground 'font-lock-constant-face      "green")
+(set-face-foreground 'font-lock-variable-name-face "darkgreen")
+(set-face-foreground 'font-lock-constant-face      "darkgreen")
