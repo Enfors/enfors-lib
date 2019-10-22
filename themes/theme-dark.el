8c2525a18afd20c293f25a5d047e2f3bc75d8684
Christer Enfors <christer.enfors@gmail.com>
2019-10-22 13:55:28 +0200

    Add dark theme

---
+++ themes/theme-dark.el
@@ -0,0 +1,10 @@
+(set-background-color "#000033")
+(set-foreground-color "#ccccff")
+
+;;;; Faces
+(set-face-foreground 'default                      "white")
+(set-face-foreground 'font-lock-string-face        "#cccc33")
+(set-face-foreground 'font-lock-keyword-face       "magenta")
+(set-face-foreground 'font-lock-comment-face       "cyan")
+(set-face-foreground 'font-lock-variable-name-face "green")
+(set-face-foreground 'font-lock-constant-face      "green")
