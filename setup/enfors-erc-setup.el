215a8f8989fd023762d8635829a9c93a3fae3aab
Christer Enfors <christer.enfors@gmail.com>
2019-11-06 16:33:18 +0100

    Colorize user names

--- setup/enfors-erc-setup.el
+++ setup/enfors-erc-setup.el
@@ -1,4 +1,42 @@
 ;;;; ERC
+;;; Code:
+(require 'erc)
+(set-face-foreground 'erc-input-face "yellow")
+;(set-face-foreground 'erc-nick-default-face "blue")
+
 (setq erc-hide-list '("JOIN" "PART" "QUIT")
       erc-nick      "Enfors")
+
+;;; The following section was copied from
+;;; https://www.emacswiki.org/emacs/ErcNickColors, Option 2:
+;; Pool of colors to use when coloring IRC nicks.
+(setq erc-colors-list '("red" "blue" "cyan" "green" "magenta" "red" "white"))
+;; special colors for some people
+(setq erc-nick-color-alist '(("John" . "blue")
+			     ("Bob" . "red")
+			     ))
+
+(defun erc-get-color-for-nick (nick)
+  "Gets a color for NICK. If NICK is in erc-nick-color-alist, use that color, else hash the nick and use a random color from the pool"
+  (or (cdr (assoc nick erc-nick-color-alist))
+      (nth
+       (mod (string-to-number
+	     (substring (md5 (downcase nick)) 0 6) 16)
+	    (length erc-colors-list))
+       erc-colors-list)))
+
+(defun erc-put-color-on-nick ()
+  "Modifies the color of nicks according to erc-get-color-for-nick"
+  (save-excursion
+    (goto-char (point-min))
+    (if (looking-at "<\\([^>]*\\)>")
+	(let ((nick (match-string 1)))
+	  (put-text-property (match-beginning 1) (match-end 1) 'face
+			     (cons 'foreground-color
+				   (erc-get-color-for-nick nick)))))))
+
+(add-hook 'erc-insert-modify-hook 'erc-put-color-on-nick)
+;;; End of section copied from Emacswiki
+
 (provide 'enfors-erc-setup)
+;;; enfors-erc-setup ends here
