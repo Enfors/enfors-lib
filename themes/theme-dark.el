70cb198a4dcedd3a6ee689aed6b18823b09e49db
Christer Enfors <christer.enfors@gmail.com>
2021-04-03 14:53:17 +0200

    Add magit colors to theme-dark.el

--- themes/theme-dark.el
+++ themes/theme-dark.el
@@ -9,3 +9,12 @@
 (set-face-foreground 'font-lock-comment-face       "cyan")
 (set-face-foreground 'font-lock-variable-name-face "green")
 (set-face-foreground 'font-lock-constant-face      "green")
+
+;;;; Magit
+(set-face-foreground 'magit-hash                   "purple")
+
+(set-face-foreground 'magit-diff-added             "purple")
+(set-face-foreground 'magit-diff-added-highlight   "black")
+
+(set-face-foreground 'magit-diff-base              "black")
+(set-face-foreground 'magit-diff-base-highlight    "white")
