93812f4a8e874116cc8415495905a7101ad527c8
Christer Enfors <christer.enfors@gmail.com>
2021-04-03 17:14:41 +0200

    Move magit color setup to enfors-magit-setup.el

--- setup/enfors-magit-setup.el
+++ setup/enfors-magit-setup.el
@@ -16,5 +16,15 @@
 ;; Set up a global key binding for magit-status
 (global-set-key (kbd "C-x g") 'magit-status)
 
+;; require magit so that it's loaded, so we can set the colors:
+(require 'magit)
+(set-face-foreground 'magit-hash                   "purple")
+
+(set-face-foreground 'magit-diff-added             "purple")
+(set-face-foreground 'magit-diff-added-highlight   "black")
+
+(set-face-foreground 'magit-diff-base              "black")
+(set-face-foreground 'magit-diff-base-highlight    "white")
+
 (provide 'enfors-magit-setup)
 ;;; enfors-magit-setup ends here
