571b44c7e313c29eb149b82374c369f7907bb64c
Christer Enfors <christer.enfors@gmail.com>
2021-08-05 06:20:06 +0000

    Add magit-section-highlight

--- themes/theme-dark.el
+++ themes/theme-dark.el
@@ -11,3 +11,4 @@
 (set-face-foreground 'font-lock-comment-face       "cyan")
 (set-face-foreground 'font-lock-variable-name-face "green")
 (set-face-foreground 'font-lock-constant-face      "green")
+(set-face-foreground 'magit-section-highlight      "#EE88CC")")
