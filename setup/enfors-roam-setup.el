dbb1f81865b5ad7a5fd96fd72bcae425686e7b6f
Christer Enfors <christer.enfors@gmail.com>
2021-09-16 14:37:13 +0200

    Add org-roam-dailies hotkeys

--- setup/enfors-roam-setup.el
+++ setup/enfors-roam-setup.el
@@ -2,7 +2,7 @@
              :ensure t
              :init
              (setq org-roam-v2-ack t
-                   org-roam-db-update-method 'immediate
+;                   org-roam-db-update-method 'immediate
                    org-roam-completion-everywhere t
                    org-roam-node-display-template "${title}")
              ; With tags, this should be (according to my Discourse thread:
@@ -14,8 +14,14 @@
                     ("C-c n f" . org-roam-node-find)
                     ("C-c n i" . org-roam-node-insert)
                     :map org-mode-map
-                    ("C-M-i"   . completion-at-point))
+                    ("C-M-i"   . completion-at-point)
+                    :map org-roam-dailies-map
+                    ("Y" . org-roam-dailies-capture-yesterday)
+                    ("T" . org-roam-dailies-capture-tomorrow))
+             :bind-keymap
+             ("C-c n d" . org-roam-dailies-map)
              :config
+             (require 'org-roam-dailies)  ;; Ensure keymap available
              (org-roam-setup))
 
 (provide 'enfors-roam-setup)
