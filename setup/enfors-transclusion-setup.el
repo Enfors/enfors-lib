422888ca7fe282baa678f75334363d61e1793853
Christer Enfors <christer.enfors@gmail.com>
2025-12-05 01:07:49 +0100

    Comment away face stuff

--- setup/enfors-transclusion-setup.el
+++ setup/enfors-transclusion-setup.el
@@ -10,15 +10,15 @@
   (set-face-attribute 'org-transclusion-fringe nil :foreground "green" :background "green"))
 
 ;; Force the Left Fringe to be 10 pixels wide (Right fringe 0)
-(fringe-mode '(10 . 0))
+;(fringe-mode '(10 . 0))
 
 ;; Make the Transclusion Bar bright green
-(set-face-attribute 'org-transclusion-fringe nil
-                    :foreground "#22ff22"
-                    :background "#22ff22")
+;; (set-face-attribute 'org-transclusion-fringe nil
+;;                     :foreground "#22ff22"
+;;                     :background "#22ff22")
 
-(custom-set-faces
- '(org-transclusion-keyword
-   ((t (:background "#005500" :foreground "#ffffff" :weight bold :box t)))))
+;; (custom-set-faces
+;;  '(org-transclusion-keyword
+;;    ((t (:background "#005500" :foreground "#ffffff" :weight bold :box t)))))
 
 (provide 'enfors-transclusion-setup)
