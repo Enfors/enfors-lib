3a8169a21a6f448e60a0034208abd85101f3b8c8
Christer Enfors <christer.enfors@gmail.com>
2026-03-05 14:12:12 +0100

    Add diminish

--- enfors-lib.el
+++ enfors-lib.el
@@ -1,9 +1,16 @@
 ;;;; enfors-lib/enfors-lib.el - main file.
 
+;;; Code:
+
 (or (boundp 'enfors-path)
     (setq   enfors-path   "~/devel/elisp/enfors-lib"))
 (add-to-list 'load-path enfors-path)
 (add-to-list 'load-path (concat enfors-path "/setup"))
+
+;; This mode is for clearing up the statusbar
+(use-package diminish
+  :ensure t)
+
 (add-to-list 'load-path (concat enfors-path "/ttrpg"))
 
 (setq enfors-files '("toys.el" "themes.el" "tools.el" "urpg.el" "dice.el"))
