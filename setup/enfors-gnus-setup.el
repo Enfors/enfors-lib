2f44d412db793c2edb51f4ff0d03987452e50d73
Christer Enfors <christer.enfors@gmail.com>
2026-04-28 13:07:45 +0200

    Add Gmail support to gnus

--- setup/enfors-gnus-setup.el
+++ setup/enfors-gnus-setup.el
@@ -1,7 +1,24 @@
-;;;; Gnus
+;;; enfors-gnus-setup.el -- my Gnus configuration
+;;; Commentary:
 ;;; Code:
 (require 'gnus)
-(setq gnus-select-method            '(nntp "news.gmane.io"))
+;; (setq gnus-select-method            '(nntp "news.gmane.io"))
+(setq gnus-select-method '(nnimap "gmail"
+               (nnimap-address "imap.gmail.com")
+               (nnimap-server-port "imaps")
+               (nnimap-stream ssl)))
+
+;; 2. Tell Emacs to use Gmail for sending (SMTP)
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      smtpmail-stream-type 'starttls)
+
+;; 3. Enforce the classic 72-character line limit for outgoing mail
+(add-hook 'message-mode-hook
+          (lambda ()
+            (setq fill-column 72)
+            (auto-fill-mode 1)))
 
 (setq gnus-secondary-select-methods
       '((nntp "news.eternal-september.org"
@@ -9,7 +26,7 @@
               (nntp-open-connection-function nntp-open-network-stream)
               (nntp-stream-type starttls))))
 
-;                                            (nntp-authinfo-file "~/.authinfo.gpg"))))
+;; (nntp-authinfo-file "~/.authinfo.gpg"))))
 ;; Recommended groups: comp.lang.c, comp.unix.shell
 
 
@@ -18,7 +35,9 @@
       user-mail-address "christer.enfors@gmail.com")
 
 ;; Prevent gnus from generating its own MessageIDs for news
-(setq message-required-news-headers
-      (remove 'Message-ID message-required-news-headers))
+;(setq message-required-news-headers
+;      (remove 'Message-ID message-required-news-headers))
 
 (provide 'enfors-gnus-setup)
+
+;;; enfors-gnus-setup.el ends here
