98d69e40cdca2fcaf9f8ee7d7cee55d75d53cefb
Christer Enfors <christer.enfors@gmail.com>
2026-05-01 14:50:16 +0200

    Update gnus setup

--- setup/enfors-gnus-setup.el
+++ setup/enfors-gnus-setup.el
@@ -8,17 +8,59 @@
                (nnimap-server-port "imaps")
                (nnimap-stream ssl)))
 
-;; 2. Tell Emacs to use Gmail for sending (SMTP)
+;; Tell Emacs to use Gmail for sending (SMTP)
 (setq message-send-mail-function 'smtpmail-send-it
       smtpmail-smtp-server "smtp.gmail.com"
       smtpmail-smtp-service 587
-      smtpmail-stream-type 'starttls)
+      smtpmail-stream-type 'starttls
+      gnus-use-full-window nil       ; Respect my authoritah and C-x 3
+      gnus-use-adaptive-scoring t)
 
-;; 3. Enforce the classic 72-character line limit for outgoing mail
-(add-hook 'message-mode-hook
-          (lambda ()
-            (setq fill-column 72)
-            (auto-fill-mode 1)))
+;; Email sorting
+;; 1 - Sort flat articles (and children in thread) by date, oldest first
+(setq gnus-article-sort-functions
+      '(gnus-article-sort-by-number
+        gnus-article-sort-by-date))
+
+;; 2 - Sort threads by the date of their root article, oldest first
+(setq gnus-thread-sort-functions
+      '(gnus-thread-sort-by-number
+        gnus-thread-sort-by-date))
+
+;; Turn off all the annoying HTML colors
+
+;; 1. The "Prefer Plain Text" Rule
+;; Most modern emails are sent as "multipart/alternative" (they contain both text and HTML).
+;; This tells Gnus to actively despise the HTML part and exclusively load the plain text.
+(setq mm-discouraged-alternatives '("text/html" "text/richtext"))
+
+;; 2. Tame the HTML Renderer (For HTML-only emails)
+;; If an email is *only* HTML, Gnus has no choice but to render it.
+;; These settings strip out all the CSS colors, fonts, and images,
+;; forcing the HTML to look exactly like standard, boring Emacs text.
+(setq shr-use-colors nil)
+(setq shr-use-fonts nil)
+(setq shr-inhibit-images t)
+
+;; Optional: If an HTML email has annoying clickable link markers, this tones them down.
+(setq shr-bullet "• ")
+
+;; Posting styles
+(setq gnus-posting-styles
+      '((".*"  ; The default
+         (eval (visual-line-mode 1))
+         (eval (auto-fill-mode -1)))
+        ("^gnu\\." ; The override for old style formatting
+         (eval (visual-line-mode -1))
+         (eval (auto-fill-mode 1))
+         (fill-column 72))))
+
+;; This version (which I no longer want) forces all email lines to be
+;; maximum 72 characters long.
+;; (add-hook 'message-mode-hook
+;;           (lambda ()
+;;             (setq fill-column 72)
+;;             (auto-fill-mode 1)))
 
 (setq gnus-secondary-select-methods
       '((nntp "news.gmane.io")
