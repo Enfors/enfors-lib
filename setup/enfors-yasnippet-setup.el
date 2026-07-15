0ebf26167704a99ff453378a04dc0ceb6b70030b
Christer Enfors <christer.enfors@gmail.com>
2026-07-15 11:42:06 +0200

    Convert yasnippet to use-package, add snippets

--- setup/enfors-yasnippet-setup.el
+++ setup/enfors-yasnippet-setup.el
@@ -1,9 +1,13 @@
 ;;; enfors-yasnippet-setup.el --- Yasnippet setup
 ;;; Commentary:
 ;;; Code:
-(require 'yasnippet)
-(yas-global-mode 1)
-(require 'warnings)
-(add-to-list 'warning-suppress-types '(yasnippet backquote-change))
+(use-package yasnippet
+  :ensure t
+  :custom
+  (yas-snippet-dirs (list (expand-file-name "snippets" enfors-path)))
+  :config
+  (yas-global-mode 1)
+  (add-to-list 'warning-suppress-types '(yasnippet backquote-change)))
+
 (provide 'enfors-yasnippet-setup)
 ;;; enfors-yasnippet-setup.el ends here
