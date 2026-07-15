3d2498edd022abeac4e66502ffcf6699eff22dc3
Christer Enfors <christer.enfors@gmail.com>
2026-07-15 09:51:39 +0200

    Improve formatting of enfors-yasnippet-setup

--- setup/enfors-yasnippet-setup.el
+++ setup/enfors-yasnippet-setup.el
@@ -1,9 +1,9 @@
-;;;; enfors-yasnippet-setup.el by Christer Enfors
-
+;;; enfors-yasnippet-setup.el --- Yasnippet setup
+;;; Commentary:
 ;;; Code:
-
 (require 'yasnippet)
 (yas-global-mode 1)
 (require 'warnings)
 (add-to-list 'warning-suppress-types '(yasnippet backquote-change))
 (provide 'enfors-yasnippet-setup)
+;;; enfors-yasnippet-setup.el ends here
